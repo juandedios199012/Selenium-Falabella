a8a79ec8f9f5da8c939832139e0b603f6558a526
Juan De Dios <jbaudazio@gmail.com>
2024-01-07 23:57:38 -0500

    eliminar el chromedriver.exe por la libreria Java Open Source: WebDriverManager

--- src/main/java/factory/ChromeDriverCreator.java
+++ src/main/java/factory/ChromeDriverCreator.java
@@ -1,5 +1,6 @@
 package factory;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,8 +9,9 @@ public class ChromeDriverCreator extends WebDriverCreator {
 
     @Override
     public WebDriver createWebDriver() {
-        System.setProperty("webdriver.chrome.driver","resource/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
+        WebDriverManager.chromedriver().clearDriverCache().setup();
+        WebDriverManager.chromedriver().setup();
         options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation","load-extension"});
 
         return new ChromeDriver(options);
